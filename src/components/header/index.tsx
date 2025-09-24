c872d7e34bd7a34e27e36447903331305db687a7
Hai Le <duchai.le@migitek.com>
2025-09-24 22:13:51 +0700

    remove header dropdown list

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,20 @@
-import { NavBar, Popover, Space, Toast } from "antd-mobile";
-import { useLocation, useNavigate } from "react-router-dom";
-import { LeftOutline, MoreOutline } from "antd-mobile-icons";
-import { titleMap } from "routes/routes";
-import "./style.scss";
+import { NavBar, Popover, Space, Toast } from "antd-mobile"
+import { useLocation, useNavigate } from "react-router-dom"
+import { LeftOutline, MoreOutline } from "antd-mobile-icons"
+import { titleMap } from "routes/routes"
+import "./style.scss"
 
 export default function Header() {
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
-  const isHome = pathname === "/";
-  const title = titleMap[pathname] || "Home";
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+  const isHome = pathname === "/"
+  const title = titleMap[pathname] || "Home"
 
   const actions = [
     { key: "a", text: "Nội dung A" },
     { key: "b", text: "Nội dung B" },
     { key: "c", text: "Nội dung C" },
-  ];
+  ]
 
   return (
     <NavBar
@@ -22,27 +22,11 @@ export default function Header() {
       backIcon={isHome ? null : <LeftOutline className="icon" />}
       onBack={() => navigate(-1)}
       left={<span className="title">{title}</span>}
-      right={
-        <Space style={{ "--gap": "14px", alignItems: "center" }}>
-          {!isHome && (
-            <Popover.Menu
-              actions={actions}
-              placement="bottom-end"
-              onAction={(node) => {
-                Toast.show({ content: `Bạn chọn: ${node.text}` });
-              }}
-              trigger="click"
-            >
-              <MoreOutline fontSize={20} />
-            </Popover.Menu>
-          )}
-        </Space>
-      }
       style={{
         borderBottom: "1px solid red",
       }}
     >
       {title}
     </NavBar>
-  );
+  )
 }
