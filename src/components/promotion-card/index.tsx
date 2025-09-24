bd46e7db1e4f6e77689d4fbaead66b1baf0a9874
Hai Le <duchai.le@migitek.com>
2025-09-24 22:11:05 +0700

    Update modal

--- src/components/promotion-card/index.tsx
+++ src/components/promotion-card/index.tsx
@@ -2,10 +2,18 @@ import { type PromotionCard as PromotionCardType } from "types/promotion-card"
 import "./style.scss"
 import { Button, Card, Space } from "antd-mobile"
 import { useNavigate } from "react-router-dom"
+import ConfirmRegisterModal from "components/confirm-register-modal"
+import { useState } from "react"
 
 export default function PromotionCard(props: PromotionCardType) {
   const { title, promotionName, duration, data, cost, utilities } = props
   const navigate = useNavigate()
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleModalClose = () => {
+    setIsModalOpen(false)
+  }
   return (
     <div className="promotion-card">
       <div className="title">{title}</div>
@@ -40,12 +48,22 @@ export default function PromotionCard(props: PromotionCardType) {
 
           <Space className="action" direction="vertical">
             <span className="cost">{cost}</span>
-            <Button className="register" color="primary" fill="outline">
+            <Button
+              className="register"
+              color="primary"
+              fill="outline"
+              onClick={(e) => {
+                e.stopPropagation()
+                setIsModalOpen(true)
+              }}
+            >
               Đăng ký
             </Button>
           </Space>
         </Space>
       </Card>
+
+      <ConfirmRegisterModal isModalOpen={isModalOpen} onModalClose={handleModalClose} />
     </div>
   )
 }
