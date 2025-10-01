b563ae269ce4a68d63ff5258e9046b8b5b499812
Nguyễn Thế Đạt <nguyenthedat@techasians.com>
2025-10-01 09:38:21 +0700

    update style mainlayout

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,23 +1,10 @@
-import Header from "components/header";
 import { Outlet } from "react-router-dom";
 import "./style.scss";
 
 export default function MainLayout() {
   return (
     <div className="main-layout">
-      {/* <div style={{ background: "#ace0ff" }}>
-        <SafeArea position="top" />
-      </div> */}
-
-      {/* Content */}
-      {/* <div className="main-content"> */}
       <Outlet />
-      {/* </div> */}
-
-      {/* <SafeArea position="bottom" />
-      <div style={{ background: "#ffcfac" }}>
-        <SafeArea position="bottom" />
-      </div> */}
     </div>
   );
 }
